6f73d5a86baabfc1e7a771d4005a4ca329ded9b1
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-30 18:07:06 +0530

    Add project files.

---
+++ ConstructorDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oops_project
+{
+    class ConstructorDemo
+    {
+        public ConstructorDemo()//explicit constructor
+        {
+            Console.WriteLine("constructor is called");
+        }
+        public void Demo()
+        {
+            Console.WriteLine("method is called");
+        }
+      public  static void Main()
+        {
+            ConstructorDemo cd1 = new ConstructorDemo();
+            
+            ConstructorDemo cd2 = new ConstructorDemo ();
+           
+            ConstructorDemo cd3 = new ConstructorDemo();
+            cd3 = cd2;
+            cd1.Demo();
+            Console.ReadLine();
+        }
+    }
+}
+    
+
